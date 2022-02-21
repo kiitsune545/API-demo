0aae449585046cf9ca243693a50717ff03ac322c
kiitsune <arvindathou6@gmail.com>
2022-02-22 02:50:29 +0530

    update index.js

--- index.js
+++ index.js
@@ -1,16 +1,40 @@
 const express = require('express');
 const app = express();
 
+app.use(express.json());
+
+const courses = [
+    { id:1, name: 'courses1'},
+    { id:2, name: 'courses2'},
+    { id:3, name: 'courses3'},
+];
+
 app.get('/', (req,res) =>{
     res.send('Hello World!!!');
 });
 
 app.get('/api/courses', (req,res) =>{
-    res.send([1, 2, 3])
+    res.send(courses)
 });
 
 app.get('/api/courses/:id', (req, res) =>{
-    res.send(req.params.id);
+    const course = courses.find(c => c.id === parseInt(req.params.id));
+    if (!course) res.status(404).send('Not Found!');
+    res.send(course);
+});
+
+app.post('/api/courses', (req,res)=>{
+    if (!req.body.name || req.body.name.length < 4){
+        res.status(400).send('Name should be minimum 4 characters');
+        return;
+    }
+    
+    const course= {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    courses.push(course);
+    res.send(course);
 });
 
 const port = process.env.PORT || 3000;
